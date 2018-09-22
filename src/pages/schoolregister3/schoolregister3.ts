60778db64c5bd3f5129f5f08dcd6d0cf5a07ff85
HAMZA-PC\Hamza Mehmood <hamza_mehmood40@hotmail.com>
2018-09-22 23:21:39 +0500

    dsf

--- src/pages/schoolregister3/schoolregister3.ts
+++ src/pages/schoolregister3/schoolregister3.ts
@@ -97,7 +97,7 @@ export class Schoolregister3Page {
       showBackdrop: false,
       content: `
       <div class="custom-spinner-container" style="width:30px">
-      <img src = "./assets/imgs/loader.gif">
+      <img src = "../../assets/imgs/loader2.gif">
       </div>`
     });
     return loader;
