55d55b83a4317c04c74e51b66ff94364787e7f2a
DESKTOP-TO9RGGN\hammad <hammad.ariez@gmail.com>
2018-09-30 01:28:37 +0500

    no message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,22 +19,91 @@ export class HomePage {
   loader: any;
   teacherList: any;
   userDetail: any;
+  searchCriteria: any;
   LogoUrl = AppSettings.LogoUrl;
   constructor(public navCtrl: NavController,public loadingCtrl: LoadingController,
     public services: Services, private storage: MyStorage,
     public tools: MyTools,
     private alertCtrl: AlertController) {
-      this.teacherService();
+      //this.teacherService();
       this.storage.get('user').then(
         (val) => {
           if (val != null) {
-            console.log('val',val);
             this.userDetail = val;
             
           }
         }
       )
+      this.storage.get('searchCriteria').then(
+        (val) => {
+          if (val != null) {
+            console.log('val',val);
+            this.searchCriteria = val;
+            if(val.Type=='Days')
+              this.matchByDay();
+            else
+            this.matchByPeriod();
+          }
+        }
+      )
   }
+
+  
+  matchByDay(){
+    
+    let body = new FormData();
+    body.append('Day', this.searchCriteria.Day);
+    body.append('TimeSlote', this.searchCriteria.TimeSlote);
+    body.append('Standard', this.searchCriteria.Standard);
+    this.services.getMatchesByDay(body).subscribe(
+      //Successfully Logged in
+      success => {
+        console.log('Success : ',success);
+        this.teacherList = success.data;
+      },
+      error => {
+        console.log('error bhai', error);
+        setTimeout(() => {
+          // if (error.message.length==1){
+            this.presentAlert('Alert!', error.message);
+            this.loader.dismiss();
+          // }
+          
+        }, 500);
+      }
+    )
+    
+  }
+
+
+  matchByPeriod(){
+    
+    let body = new FormData();
+    body.append('ToDate', this.searchCriteria.ToDate);
+    body.append('FromDate', this.searchCriteria.FromDate);
+    body.append('TimeSlote', this.searchCriteria.TimeSlote);
+    body.append('Standard', this.searchCriteria.Standard);
+    this.services.getMatchesByPeriod(body).subscribe(
+      //Successfully Logged in
+      success => {
+        console.log('Success : ',success);
+        this.teacherList = success.data;
+      },
+      error => {
+        console.log('error bhai', error);
+        setTimeout(() => {
+          // if (error.message.length==1){
+            this.presentAlert('Alert!', error.message);
+            this.loader.dismiss();
+          // }
+          
+        }, 500);
+      }
+    )
+    
+   }
+
+
   presentAlert(title1,msgs) {
     let alert = this.alertCtrl.create({
       title: title1,
@@ -57,8 +126,6 @@ teacherService() {
   
    
   //Requesting API 
-
-        
       this.services.getTeachers().subscribe(
         //Successfully Logged in
         success => {
@@ -103,6 +170,10 @@ profilepage(idd){
   this.navCtrl.push(PublicprofilePage,{
     id: idd
   })
+}
+
+inviteTeacher(){
+  
 }
 
 }
